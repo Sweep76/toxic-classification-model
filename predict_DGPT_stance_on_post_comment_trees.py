276cbefa8516a4c66bfa28c6bb7d5d3c4924dbdc
Sweep <joshuapatrickchiu76@gmail.com>
2024-10-19 19:51:12 +0800

    feat: GPT-2 model setup for stance classification

--- predict_DGPT_stance_on_post_comment_trees.py
+++ predict_DGPT_stance_on_post_comment_trees.py
@@ -7,3 +7,91 @@ import utils
 from utils import RANDOM_SEED, log_list, print_list, make_dir_if_not_exists, save_in_pickle, load_from_pickle, save_in_json, load_from_json, format_time, plot_train_loss, log_TP_FP_FN_TN_from_binary_predictions, draw_and_save_precision_recall_curve, save_list_of_tuples_to_tsv
 import pdb
 
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Model, GPT2Config, AdamW, get_linear_schedule_with_warmup,  AutoModelForCausalLM, AutoTokenizer
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torch import autograd
+import torch
+torch.manual_seed(RANDOM_SEED+1)
+
+import random
+random.seed(RANDOM_SEED)
+
+import os
+import re
+import math
+import time
+import copy
+import ast
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+from collections import Counter
+from sklearn import metrics
+
+from sklearn.metrics import average_precision_score, precision_recall_curve
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# Import functions and classes specific to SBF training and evaluation.
+from SBF_utils import BertForSBF, SBF_BERT_Dataset, count_unique_posts, convert_string_label_to_binary, relabel_with_binarized_votes_and_create_BERT_instances, get_labels_dict_from_list
+# Import functions and classes specific to OC_S training and evaluation.
+from OC_S_utils import Conversation_Data, get_conversation_data_from_OC_S_file, get_save_lists_from_conv_data, OC_S_stance_Dataset, get_conversation_data_from_SBF_instances, log_TP_FP_FN_TN_from_conv_off_predictions, TARGET_GROUPS, TARGET_GROUPS_TO_ID, log_TP_FP_FN_TN_convs_from_stance_predictions, log_top_conv_stance_predictions
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_file", help="Path to the pickle file containing the post-threads on which we want to make predictions", type=str, required=True)
+parser.add_argument("-sm", "--stance_model_dir", help="Path to the directory containing trained DGPT stance model and its tokenizer", type=str, required=True)
+parser.add_argument("-om", "--offensive_model_dir", help="Path to the directory containing trained DGPT offensive model and its tokenizer", type=str, required=True)
+parser.add_argument("-o", "--output_dir", help="Path to the output directory where we will save all the model prediction and results", type=str, required=True)
+parser.add_argument("-s", "--save_file", help="Optional path to the output save file to save all the model prediction and results", type=str, default="")
+parser.add_argument("-bs", "--batch_size", help="Train batch size for GPT2 model", type=int, default=32)
+args = parser.parse_args()
+
+
+import logging
+# Ref: https://stackoverflow.com/a/49202811/4535284
+for handler in logging.root.handlers[:]:
+	logging.root.removeHandler(handler)
+# Also add the stream handler so that it logs on STD out as well
+# Ref: https://stackoverflow.com/a/46098711/4535284
+make_dir_if_not_exists(args.output_dir)
+logfile = os.path.join(args.output_dir, "output.log")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler(logfile, mode='w'), logging.StreamHandler()])
+
+# PRETRAINED_GPT2_MODEL = 'GPT2-base-cased'
+PRETRAINED_GPT2_MODEL = 'microsoft/DialoGPT-medium'
+# Other global constants required for the code
+POSSIBLE_BATCH_SIZE = 1
+MAX_SEQ_THRESH = 512
+
+if torch.cuda.is_available():
+	device = torch.device("cuda")
+	logging.info(f"Using GPU{torch.cuda.get_device_name(0)} to make predictions")
+else:
+	device = torch.device("cpu")
+	logging.info(f"Using CPU to make predictions")
+
+def reweight(cls_num_list, beta=0.9999):
+	'''
+	Implement reweighting by effective numbers
+	:param cls_num_list: a list containing # of samples of each class
+	:param beta: hyper-parameter for reweighting, see paper for more details
+	:return:
+	'''
+	per_cls_weights = None
+	#############################################################################
+	# TODO: reweight each class by effective numbers                            #
+	#############################################################################
+	n_is = np.array(cls_num_list)
+	per_cls_weights = (1 - beta) / (1 - np.power(beta, n_is))
+	per_cls_weights = torch.from_numpy(per_cls_weights)
+	# per_cls_weights = per_cls_weights / per_cls_weights.sum() * 10
+	#############################################################################
+	#                              END OF YOUR CODE                             #
+	#############################################################################
+	return per_cls_weights
