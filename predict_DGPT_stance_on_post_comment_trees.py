0981fb23236460fc8ae5ae21d8a3b7c0e503a1e5
Sweep <joshuapatrickchiu76@gmail.com>
2024-10-20 22:25:51 +0800

    Defines the forward pass with multiple input types

--- predict_DGPT_stance_on_post_comment_trees.py
+++ predict_DGPT_stance_on_post_comment_trees.py
@@ -152,4 +152,52 @@ class GPT2ForOC_S_stance(GPT2LMHeadModel):
 		# self.target_loss_fct = nn.BCEWithLogitsLoss()
 		# self.stance_loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 100.0, 100.0]))
 		# self.stance_loss_multiplier = 2.0
+	
+	def forward(
+			self,
+			input_ids,
+			eos_toward_token_ids=None,
+			eos_response_token_ids=None,
+			attention_mask=None,
+			token_type_ids=None,
+			position_ids=None,
+			head_mask=None,
+			inputs_embeds=None,
+			stance_labels=None,
+			# off_labels=None,
+			# target_labels=None,
+		):
+			r"""
+			labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`, defaults to :obj:`None`):
+				Labels for computing the sequence classification/regression loss.
+				Indices should be in :obj:`[0, ..., config.num_labels - 1]`.
+				If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
+				If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+
+		Returns:
+			:obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.GPT2Config`) and inputs:
+			loss (:obj:`torch.FloatTensor` of shape :obj:`(1,)`, `optional`, returned when :obj:`label` is provided):
+				Classification (or regression if config.num_labels==1) loss.
+			logits (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, config.num_labels)`):
+				Classification (or regression if config.num_labels==1) scores (before SoftMax).
+			hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_hidden_states=True``):
+				Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+				of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+				Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+			attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_attentions=True``):
+				Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+				:obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+				Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+				heads.
+			"""
+			outputs = self.transformer(
+				input_ids,
+				attention_mask=attention_mask,
+				token_type_ids=token_type_ids,
+				position_ids=position_ids,
+				head_mask=head_mask,
+				inputs_embeds=inputs_embeds,
+			)
 			
